90f6802f734dabdec4c22b14151d345cf5a62de9
Hamza <hnael99@gmail.com>
2021-09-22 15:08:25 +0300

    skeleton project

---
+++ src/test/kotlin/com/colureware/chatbot/ChatbotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.colureware.chatbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatbotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
